e34535abe14b919acdf1ce0bc32f54099d31e026
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2021-10-08 10:25:27 +0700

    video 3
    menyelesaikan video ke 3 (Tugas 5)

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "nama" => "Fajar Hasbi Noer",
+        "email" => "193040076.fajar@mail.unpas.ac.id",
+        "image" => "Foto Fajar.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

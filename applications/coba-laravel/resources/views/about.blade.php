c6414841200a2b191ce73eb38363c068487d45fb
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2021-10-08 10:45:50 +0700

    video 4
    menyelesaikan video 4 (Tugas 5)

--- applications/coba-laravel/resources/views/about.blade.php
+++ applications/coba-laravel/resources/views/about.blade.php
@@ -9,8 +9,8 @@
 </head>
 <body>
     <h1>Halaman About</h1>
-    <h3><?=  $nama; ?> </h3>
-    <p><?=  $email; ?></p>
-    <img src="img/<?=  $image; ?>" alt="<?=  $nama; ?>"width="200">
+    <h3>{{ $nama }} </h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $nama }}"width="200">
 </body>
 </html>

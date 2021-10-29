30257fc4dbfde25586385d2e6cb800450b82de4b
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2021-10-29 20:45:06 +0700

    Tugas 7
    menyelesaikan video 1 (Tugas 7)

--- applications/coba-laravel/database/seeders/DatabaseSeeder.php
+++ applications/coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,66 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Fajar Hasbi Noer',
+            'email'=> 'fajarhasbinoer2@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programing',
+            'slug' => 'web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'Judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam!',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam! Quia velit quidem ipsa sunt, ex autem officiis ipsum corrupti voluptatibus. 
+            Consequatur officia repellat vel blanditiis. Architecto dolorum eaque labore est nulla libero ex repellat eveniet illo possimus sequi a sunt ratione nam odio nesciunt, reprehenderit rerum omnis dolor adipisci magni placeat? Rerum, eveniet iure provident, minus culpa voluptatum dolore magni neque itaque atque asperiores doloremque expedita nam autem esse aspernatur soluta nesciunt sequi cumque quidem. 
+            Consectetur magnam rem soluta eveniet nam mollitia! Amet nulla, fuga temporibus autem animi fugit cumque quis.
+            Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam!',
+            
+            'category_id' =>1,
+
+            'user_id' =>1
+       ]);
+
+       Post::create([
+        'title' => 'Judul Kedua',
+        'slug' => 'Judul-kedua',
+        'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam!',
+        'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam! Quia velit quidem ipsa sunt, ex autem officiis ipsum corrupti voluptatibus. 
+        Consequatur officia repellat vel blanditiis. Architecto dolorum eaque labore est nulla libero ex repellat eveniet illo possimus sequi a sunt ratione nam odio nesciunt, reprehenderit rerum omnis dolor adipisci magni placeat? Rerum, eveniet iure provident, minus culpa voluptatum dolore magni neque itaque atque asperiores doloremque expedita nam autem esse aspernatur soluta nesciunt sequi cumque quidem. 
+        Consectetur magnam rem soluta eveniet nam mollitia! Amet nulla, fuga temporibus autem animi fugit cumque quis.
+        Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam!',
+        
+        'category_id' =>1,
+
+        'user_id' =>1
+   ]);
+
+   Post::create([
+    'title' => 'Judul Ketiga',
+    'slug' => 'Judul-ketiga',
+    'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam!',
+    'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+    Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam! Quia velit quidem ipsa sunt, ex autem officiis ipsum corrupti voluptatibus. 
+    Consequatur officia repellat vel blanditiis. Architecto dolorum eaque labore est nulla libero ex repellat eveniet illo possimus sequi a sunt ratione nam odio nesciunt, reprehenderit rerum omnis dolor adipisci magni placeat? Rerum, eveniet iure provident, minus culpa voluptatum dolore magni neque itaque atque asperiores doloremque expedita nam autem esse aspernatur soluta nesciunt sequi cumque quidem. 
+    Consectetur magnam rem soluta eveniet nam mollitia! Amet nulla, fuga temporibus autem animi fugit cumque quis.
+    Obcaecati expedita sed quasi ut facere, earum dolorum cumque magnam!',
+    
+    'category_id' =>2,
+
+    'user_id' =>1
+]);
     }
 }

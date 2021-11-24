c4ed382f77c7bcb4a329dffab856493ec2032c66
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2021-11-24 19:36:43 +0700

    Menyelesaikan video 17 Tugas 9
    menyelesaikan video 17

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -54,23 +56,8 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function (){
+    return view('dashboard.index');
+})->middleware('auth');
 
-
-// Route::get('/categories/{category:slug}', function (Category $category) {
-//     return view('posts', [
-//         'title' => "Post by Category : $category->name",
-//         'active' => 'categories',
-//         'posts' => $category->posts->load('category', 'author')
-
-//     ]
-// });
-
-
-// Route::get('/authors/{author:username}', function (User $author) {
-//     return view('posts', [
-//         'title' => "Post By Author : $author->name",
-//         'active' => 'posts',
-//         'posts' => $author->posts->load('category', 'author'),
-//     ]);
-// });
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');

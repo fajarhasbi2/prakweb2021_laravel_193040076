af2d59657d3ec82d8a6515796246e4f7fac31e79
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2021-12-03 21:32:04 +0700

    Menyelesaikan video 23 tugas 10
    menyelesaikan video 23

--- applications/coba-laravel/app/Providers/AppServiceProvider.php
+++ applications/coba-laravel/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap();
+        Gate::define('admin', function (User $user) {
+            return $user->is_admin;
+        });
     }
 }
